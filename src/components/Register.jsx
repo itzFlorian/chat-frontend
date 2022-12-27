22e598b1c72e0453076faaa44a0bc9d8e7287ec0
Florian <f.mewes87@gmail.com>
2022-12-27 11:40:18 +0100

    pushe die User Daten zur Datenbank

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,12 +1,16 @@
+import { registerRoute } from "../routes/ApiRoutes.js";
+
 import { useState, useEffect } from "react";
 import {Link} from "react-router-dom"
+
+// IMPORT TOAST UND STANDARAUSSEHEN
 import { toast, ToastContainer } from "react-toastify";
-// IMPORT STANDARAUSSEHEN VOM TOAST
 import "react-toastify/dist/ReactToastify.css"
 //<----------------
+
 import "../styles/register.scss"
-const Register = () => {
-  
+
+const Register = () => {    
   const [input, setInput] = useState({
     username: "",
     email:"",
@@ -15,12 +19,33 @@ const Register = () => {
   })
 
   const handleSubmit = (e) => {
+    const {username, email, password} = input;
     e.preventDefault()
-    handleValidation()
-    console.log("hallo");
+    if (handleValidation()){
+      fetch(registerRoute, {
+      method: 'POST',
+      body: JSON.stringify({
+      username,
+      password,
+      email
+      }),
+      headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+  .then((response) => response.json())
+  .then((json) => console.log(json));
   }
+  setInput(prev => prev = {
+    username: "",
+    email:"",
+    password:"",
+    confirmPassword:""
+  })
+}
+
   const handleChange = (e) => {
-    setInput({...input, [e.target.name]:e.target.value})
+    setInput(prev => prev = {...input, [e.target.name]:e.target.value})
   }
 
   const toastOptions = {
@@ -28,11 +53,22 @@ const Register = () => {
     autoClose: 8000,
     theme:"dark"
   }
-
   const handleValidation = () => {
   const {username, email, password, confirmPassword} = input;
-  if(password !== confirmPassword) toast.error("password and confirm password should be the same", toastOptions);
-  if(username.length < 2) toast.error("Your username must have at least 2 characters", toastOptions)
+  if(password !== confirmPassword) {
+    toast.error("password and confirm password should be the same", toastOptions);
+    return false
+  }else if(username.length < 2) {
+    toast.error("Username must have at least 2 characters", toastOptions)
+    return false
+  }else if(password.length < 6) {
+    toast.error("Password must have at least 7 charhacters", toastOptions)
+    return false
+  }else if (email === ""){
+    toast.error("email is required", toastOptions)
+    return false
+  }
+  return true
   }
 
   return (
@@ -43,17 +79,15 @@ const Register = () => {
           <img src="" alt="" />
           <h1>BrandName</h1>
         </div>
-        <input type="text" name="username" placeholder="Username" onChange={handleChange}/>
-        <input type="email" name="email" placeholder="Email" onChange={handleChange}/>
-        <input type="password" name="password" placeholder="Password" onChange={handleChange}/>
-        <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange}/>
+        <input type="text" name="username" placeholder="Username" onChange={handleChange} value={input.username}/>
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} value={input.email}/>
+        <input type="password" name="password" placeholder="Password" onChange={handleChange} value={input.password}/>
+        <input type="password" name="confirmPassword" placeholder="Confirm Password" onChange={handleChange} value={input.confirmPassword}/>
         <button type="submit">Submit</button>
         <p>Already have an Account ? <Link to ="/login"> Login </Link></p>
       </form> 
     </div>
-    <ToastContainer>
-
-    </ToastContainer>
+    <ToastContainer/>
     </>
   );
 };
---
+++ src/routes/ApiRoutes.js
@@ -0,0 +1,2 @@
+const host = "http://localhost:7000"
+export const registerRoute = `${host}/users`
